9a9f7dc631bf9b39855af9d9429f003efb763076
Fabian Seitz <seitzf1@yahoo.de>
2026-04-09 08:51:03 +0200

    ruff linter fix

--- tests/test_coordinator.py
+++ tests/test_coordinator.py
@@ -2,8 +2,9 @@
 
 from __future__ import annotations
 
-import pytest
 from unittest.mock import AsyncMock, MagicMock, patch
+
+import pytest
 from homeassistant.helpers.update_coordinator import UpdateFailed
 
 from custom_components.openwrt.api.base import OpenWrtData, SystemResources
